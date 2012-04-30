fa1d865b72034f87610a4d09548d47190cf93a66
Pascal Hartig <phartig@rdrei.net>
2012-04-30 16:59:17 +0200

    Added correct soundcloud API SSL pin

--- scdl/src/main/java/net/rdrei/android/scdl/Config.java
+++ scdl/src/main/java/net/rdrei/android/scdl/Config.java
@@ -2,5 +2,5 @@ package net.rdrei.android.scdl;
 
 public interface Config {
 	String API_CONSUMER_KEY = "429caab2811564cb27f52a7a4964269b";
-	String API_SSL_PIN_HASH = "2b6df9cdd2367145d2ea1a7850da5fc62b6ff29b";
+	String API_SSL_PIN_HASH = "970cc50da37b10f0d82e0db2b47952d1af50c81f";
 }
