6bb743976643b7914e99755f230f0cdaae32334d
Pascal Hartig <phartig@rdrei.net>
2012-05-09 11:24:55 +0200

    Added example options to download location settings

---
+++ scdl/src/main/java/net/rdrei/android/scdl/ApplicationPreferences.java
@@ -0,0 +1,8 @@
+package net.rdrei.android.scdl;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class ApplicationPreferences {
+	public final static String KEY_STORAGE_TYPE = "download_preferences_storage_type";
+}
--- scdl/src/main/java/net/rdrei/android/scdl/ui/DownloadPreferencesFragment.java
+++ scdl/src/main/java/net/rdrei/android/scdl/ui/DownloadPreferencesFragment.java
@@ -1,7 +1,10 @@
 package net.rdrei.android.scdl.ui;
 
+import net.rdrei.android.scdl.ApplicationPreferences;
 import net.rdrei.android.scdl.R;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 public class DownloadPreferencesFragment extends PreferenceFragment {
@@ -13,7 +16,15 @@ public class DownloadPreferencesFragment extends PreferenceFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		addPreferencesFromResource(R.xml.download_preferences);
+
+		loadStorageTypeOptions();
+	}
+
+	private void loadStorageTypeOptions() {
+		ListPreference typePreference = (ListPreference) findPreference(ApplicationPreferences.KEY_STORAGE_TYPE);
+		typePreference.setEntries(new CharSequence[] {"SD Card", "Phone", "Custom Path"});
+		typePreference.setEntryValues(R.array.storage_types);
 	}
 }
