53750dbbd371a5f7b1f7707311c1709d3c015bd9
Pascal Hartig <phartig@rdrei.net>
2012-05-10 00:16:37 +0200

    Display free space in preferences

--- scdl/src/main/java/net/rdrei/android/scdl/ui/DownloadPreferencesFragment.java
+++ scdl/src/main/java/net/rdrei/android/scdl/ui/DownloadPreferencesFragment.java
@@ -3,6 +3,8 @@ package net.rdrei.android.scdl.ui;
 import net.rdrei.android.scdl.ApplicationPreferences;
 import net.rdrei.android.scdl.R;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -24,7 +26,35 @@ public class DownloadPreferencesFragment extends PreferenceFragment {
 
 	private void loadStorageTypeOptions() {
 		ListPreference typePreference = (ListPreference) findPreference(ApplicationPreferences.KEY_STORAGE_TYPE);
-		typePreference.setEntries(new CharSequence[] {"SD Card", "Phone", "Custom Path"});
+		typePreference.setEntries(new CharSequence[] { getExternalLabel(),
+				getPhoneLabel(), "Custom Path" });
 		typePreference.setEntryValues(R.array.storage_types);
+	}
+
+	private String getExternalLabel() {
+		return String.format("SD Card (%.2fGB free)", getFreeExternalStorage()
+				/ Math.pow(1024, 3));
+	}
+
+	private String getPhoneLabel() {
+		return String.format("Phone (%.2fGB free)", getFreeInternalStorage()
+				/ Math.pow(1024, 3));
+	}
+
+	/**
+	 * Returns the free bytes on external storage.
+	 */
+	public static long getFreeExternalStorage() {
+		StatFs statFs = new StatFs(Environment.getExternalStorageDirectory()
+				.getPath());
+		return statFs.getAvailableBlocks() * statFs.getBlockSize();
+	}
+
+	/**
+	 * Returns the free bytes on internal storage.
+	 */
+	public static long getFreeInternalStorage() {
+		StatFs statFs = new StatFs(Environment.getDataDirectory().getPath());
+		return statFs.getAvailableBlocks() * statFs.getBlockSize();
 	}
 }
