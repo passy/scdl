c261d72933f29a7251ee1646d116475f10bb779c
Pascal Hartig <phartig@rdrei.net>
2012-05-10 23:41:20 +0200

    Made download querying async

--- scdl/src/main/java/net/rdrei/android/scdl/receiver/DownloadCompleteReceiver.java
+++ scdl/src/main/java/net/rdrei/android/scdl/receiver/DownloadCompleteReceiver.java
@@ -3,6 +3,7 @@ package net.rdrei.android.scdl.receiver;
 import net.rdrei.android.scdl.R;
 import roboguice.receiver.RoboBroadcastReceiver;
 import roboguice.util.Ln;
+import roboguice.util.RoboAsyncTask;
 import android.app.DownloadManager;
 import android.app.DownloadManager.Query;
 import android.app.Notification;
@@ -28,37 +29,39 @@ public class DownloadCompleteReceiver extends RoboBroadcastReceiver {
 	@Inject
 	private NotificationManager mNotificationManager;
 
+	/**
+	 * Simple POJO for passing around download information.
+	 * 
+	 * @author pascal
+	 * 
+	 */
+	private class Download {
+		private String mTitle;
+		private int mStatus;
+
+		public String getTitle() {
+			return mTitle;
+		}
+
+		public void setTitle(String title) {
+			mTitle = title;
+		}
+
+		public int getStatus() {
+			return mStatus;
+		}
+
+		public void setStatus(int status) {
+			mStatus = status;
+		}
+	}
+
 	@Override
 	public void handleReceive(Context context, Intent intent) {
 		long downloadId = intent.getLongExtra(
 				DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-
-		final Query query = new DownloadManager.Query();
-		query.setFilterById(downloadId);
-		final Cursor cursor = mDownloadManager.query(query);
-
-		if (!cursor.moveToFirst()) {
-			// Download could not be found.
-			Ln.d("Could not find download with id %d.", downloadId);
-			return;
-		}
-
-		final int descriptionIndex = cursor
-				.getColumnIndex(DownloadManager.COLUMN_DESCRIPTION);
-
-		if (!cursor.getString(descriptionIndex).equals(
-				context.getString(R.string.download_description))) {
-			// Download doesn't belong to us. Weird way to check, but way, way
-			// easier than keeping track of the IDs.
-			Ln.d("Description did not match SCDL default description.");
-			return;
-		}
-
-		final int titleIndex = cursor
-				.getColumnIndex(DownloadManager.COLUMN_TITLE);
-		final String title = cursor.getString(titleIndex);
-
-		showNotification(context, title);
+		ResolveDownloadTask task = new ResolveDownloadTask(context, downloadId);
+		task.execute();
 	}
 
 	/**
@@ -85,5 +88,62 @@ public class DownloadCompleteReceiver extends RoboBroadcastReceiver {
 				.getNotification();
 
 		mNotificationManager.notify(0, notification);
+	}
+
+	private class ResolveDownloadTask extends RoboAsyncTask<Download> {
+		private final long mDownloadId;
+
+		public ResolveDownloadTask(Context context, long downloadId) {
+			super(context);
+
+			mDownloadId = downloadId;
+		}
+
+		@Override
+		public Download call() throws Exception {
+			final Query query = new DownloadManager.Query();
+			query.setFilterById(mDownloadId);
+			final Cursor cursor = mDownloadManager.query(query);
+
+			if (!cursor.moveToFirst()) {
+				// Download could not be found.
+				Ln.d("Could not find download with id %d.", mDownloadId);
+				return null;
+			}
+
+			final int descriptionIndex = cursor
+					.getColumnIndex(DownloadManager.COLUMN_DESCRIPTION);
+
+			if (!cursor.getString(descriptionIndex).equals(
+					context.getString(R.string.download_description))) {
+				// Download doesn't belong to us. Weird way to check, but way,
+				// way
+				// easier than keeping track of the IDs.
+				Ln.d("Description did not match SCDL default description.");
+				return null;
+			}
+
+			final int titleIndex = cursor
+					.getColumnIndex(DownloadManager.COLUMN_TITLE);
+			final String title = cursor.getString(titleIndex);
+
+			final int statusIndex = cursor
+					.getColumnIndex(DownloadManager.COLUMN_STATUS);
+			final int status = cursor.getInt(statusIndex);
+
+			final Download download = new Download();
+			download.setTitle(title);
+			download.setStatus(status);
+
+			return download;
+		}
+
+		@Override
+		protected void onSuccess(Download t) throws Exception {
+			super.onSuccess(t);
+
+			showNotification(context, t.getTitle());
+		}
+
 	}
 }
