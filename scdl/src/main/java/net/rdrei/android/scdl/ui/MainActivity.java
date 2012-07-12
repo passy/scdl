39e396dd216bc8226308589dd746ed4cacad3ca4
Pascal Hartig <phartig@rdrei.net>
2012-07-12 23:45:25 +0200

    Fixed imports in MainActivity

--- scdl/src/main/java/net/rdrei/android/scdl/ui/MainActivity.java
+++ scdl/src/main/java/net/rdrei/android/scdl/ui/MainActivity.java
@@ -5,18 +5,22 @@ import net.rdrei.android.scdl.ui.TrackErrorActivity.ErrorCode;
 import roboguice.activity.RoboFragmentActivity;
 import roboguice.inject.InjectView;
 import roboguice.util.Ln;
+import sheetrock.panda.changelog.ChangeLog;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
-import android.app.AlertDialog.Builder;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import sheetrock.panda.changelog.ChangeLog;
 import com.google.inject.Inject;
 import com.viewpagerindicator.CirclePageIndicator;
 
