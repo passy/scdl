f176adccf2c530add4ac5414027fcccfbf0528a9
Pascal Hartig <passy@twitter.com>
2013-11-02 16:12:13 +0000

    Works

---
+++ scdl/src/main/java/net/rdrei/android/scdl2/ui/AbstractPendingDownloadResolver.java
@@ -0,0 +1,60 @@
+package net.rdrei.android.scdl2.ui;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.google.inject.Inject;
+
+import net.rdrei.android.scdl2.ShareIntentResolver;
+import net.rdrei.android.scdl2.api.PendingDownload;
+
+import roboguice.inject.ContextScope;
+import roboguice.util.RoboAsyncTask;
+
+/**
+ * Resolves an Intent URL to a pending download.
+ */
+public abstract class AbstractPendingDownloadResolver extends RoboAsyncTask<PendingDownload> {
+	@Inject
+	private ShareIntentResolver mShareIntentResolver;
+
+	@Inject
+	private ContextScope mContextScope;
+
+	public AbstractPendingDownloadResolver(Context context, Handler handler) {
+		super(context, handler);
+	}
+
+	@Override
+	public PendingDownload call() throws Exception {
+		mContextScope.enter(context);
+		try {
+			return mShareIntentResolver.resolvePendingDownload();
+		} finally {
+			mContextScope.exit(context);
+		}
+	}
+
+	@Override
+	protected void onException(final Exception e) throws RuntimeException {
+		super.onException(e);
+		final TrackErrorActivity.ErrorCode errorCode;
+
+		if (e instanceof ShareIntentResolver.UnsupportedUrlException) {
+			errorCode = TrackErrorActivity.ErrorCode.UNSUPPORTED_URL;
+		} else if (e instanceof ShareIntentResolver.TrackNotFoundException) {
+			errorCode = TrackErrorActivity.ErrorCode.NOT_FOUND;
+		} else if (e instanceof ShareIntentResolver.UnsupportedPlaylistUrlException) {
+			errorCode = TrackErrorActivity.ErrorCode.PLAYLIST;
+		} else {
+			errorCode = TrackErrorActivity.ErrorCode.NETWORK_ERROR;
+		}
+
+		onErrorCode(errorCode);
+	}
+
+	protected abstract void onErrorCode(final TrackErrorActivity.ErrorCode errorCode);
+
+	@Override
+	protected abstract void onSuccess(final PendingDownload download) throws Exception;
+}
--- scdl/src/main/java/net/rdrei/android/scdl2/ui/DownloadActivity.java
+++ scdl/src/main/java/net/rdrei/android/scdl2/ui/DownloadActivity.java
@@ -1,12 +1,16 @@
 package net.rdrei.android.scdl2.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import net.rdrei.android.scdl2.R;
+import net.rdrei.android.scdl2.api.MediaState;
+import net.rdrei.android.scdl2.api.PendingDownload;
 
-import roboguice.activity.RoboActivity;
 import roboguice.activity.RoboFragmentActivity;
+import roboguice.util.Ln;
 
 /**
  * Dispatches a download request to either the single download or playlist download activity.
@@ -15,6 +19,8 @@ public class DownloadActivity extends RoboFragmentActivity {
 	public static final String ANALYTICS_TAGS = "DOWNLOAD_ACTIVITY";
 	public static final String MEDIA_STATE_TAG = "scdl:MEDIA_STATE_TAG";
 
+	private MediaState mMediaState;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,10 +28,53 @@ public class DownloadActivity extends RoboFragmentActivity {
 		setContentView(R.layout.download);
 
 		if (savedInstanceState != null) {
+			Ln.d("Loading previous media state.");
 			// TODO: Load MediaState
 		} else {
 			CommonMenuFragment.injectMenu(this);
+		}
+
+		if (mMediaState == null) {
+			Ln.d("No previous state. Starting media resolver.");
+			final AbstractPendingDownloadResolver task = new PendingDownloadResolver(this);
 			// TODO: Resolve MediaState
+			task.execute();
+		} else {
+			loadMediaFragments();
+		}
+	}
+
+	protected void loadMediaFragments() {
+		Ln.w("loadMediaFragments: Not implemented.");
+	}
+
+	/**
+	 * Show error activity with the given error code and exit the current
+	 * activity.
+	 *
+	 * @param errorCode
+	 */
+	protected void startErrorActivity(
+			final TrackErrorActivity.ErrorCode errorCode) {
+		final Intent intent = new Intent(this, TrackErrorActivity.class);
+		intent.putExtra(TrackErrorActivity.EXTRA_ERROR_CODE, errorCode);
+		startActivity(intent);
+		finish();
+	}
+
+	private class PendingDownloadResolver extends AbstractPendingDownloadResolver {
+		public PendingDownloadResolver(Context context, Handler handler) {
+			super(context, handler);
+		}
+
+		@Override
+		protected void onErrorCode(TrackErrorActivity.ErrorCode errorCode) {
+			startErrorActivity(errorCode);
+		}
+
+		@Override
+		protected void onSuccess(PendingDownload download) throws Exception {
+			// TODO: Call Track/Playlist resolver task.
 		}
 	}
 }
