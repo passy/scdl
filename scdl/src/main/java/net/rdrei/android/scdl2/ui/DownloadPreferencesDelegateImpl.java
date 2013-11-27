d443c361dee76c5465ceab5d7f7225de76130ce0
Pascal Hartig <passy@twitter.com>
2013-11-27 11:28:03 +0000

    Better display of free space

--- scdl/src/main/java/net/rdrei/android/scdl2/ui/DownloadPreferencesDelegateImpl.java
+++ scdl/src/main/java/net/rdrei/android/scdl2/ui/DownloadPreferencesDelegateImpl.java
@@ -2,32 +2,35 @@ package net.rdrei.android.scdl2.ui;
 
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
-import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.StatFs;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
+import android.text.format.Formatter;
+
 import com.google.analytics.tracking.android.Tracker;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import net.rdrei.android.dirchooser.DirectoryChooserActivity;
-import net.rdrei.android.scdl2.*;
-import net.rdrei.android.scdl2.ApplicationPreferences.StorageType;
-import net.rdrei.android.scdl2.DownloadPathValidator.DownloadPathValidationException;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import net.rdrei.android.dirchooser.DirectoryChooserActivity;
+import net.rdrei.android.scdl2.ActivityStarter;
+import net.rdrei.android.scdl2.ApplicationPreferences;
+import net.rdrei.android.scdl2.ApplicationPreferences.StorageType;
+import net.rdrei.android.scdl2.Config;
+import net.rdrei.android.scdl2.DownloadPathValidator;
+import net.rdrei.android.scdl2.DownloadPathValidator.DownloadPathValidationException;
+import net.rdrei.android.scdl2.PackageHelper;
+import net.rdrei.android.scdl2.PreferenceManagerWrapper;
+import net.rdrei.android.scdl2.R;
 
 import roboguice.util.Ln;
 
-public class DownloadPreferencesDelegateImpl implements
-		OnSharedPreferenceChangeListener, DownloadPreferencesDelegate {
+public class DownloadPreferencesDelegateImpl implements OnSharedPreferenceChangeListener, DownloadPreferencesDelegate {
 
 	private static final String DOWNLOAD_DIRECTORY_NAME = "SoundCloud";
 	private static final String ANALYTICS_TAG = "DOWNLOAD_PREFERENCES";
@@ -55,13 +58,12 @@ public class DownloadPreferencesDelegateImpl implements
 	private ActivityStarter mActivityStarter;
 
 	@Inject
-	Tracker mTracker;
+	private Tracker mTracker;
 
 	private final PreferenceManagerWrapper mPreferenceManager;
 
 	@Inject
-	public DownloadPreferencesDelegateImpl(
-			@Assisted final PreferenceManagerWrapper manager) {
+	public DownloadPreferencesDelegateImpl(@Assisted final PreferenceManagerWrapper manager) {
 		mPreferenceManager = manager;
 	}
 
@@ -72,19 +74,18 @@ public class DownloadPreferencesDelegateImpl implements
 	 */
 	@Override
 	public void onCreate(final ActivityStarter activityStarter) {
-		mTypePreference = (ListPreference) mPreferenceManager
-				.findPreference(ApplicationPreferences.KEY_STORAGE_TYPE);
-		mPathPreference = mPreferenceManager
-				.findPreference(ApplicationPreferences.KEY_STORAGE_CUSTOM_PATH);
+		mTypePreference = (ListPreference) mPreferenceManager.findPreference(
+				ApplicationPreferences.KEY_STORAGE_TYPE);
+		mPathPreference = mPreferenceManager.findPreference(
+				ApplicationPreferences.KEY_STORAGE_CUSTOM_PATH);
 		mPathPreference.setOnPreferenceChangeListener(mCustomPathValidator);
-		mPathPreference
-				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(final Preference preference) {
-						startDownloadDirectoryChooser();
-						return true;
-					}
-				});
+		mPathPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(final Preference preference) {
+				startDownloadDirectoryChooser();
+				return true;
+			}
+		});
 
 		setupAboutHandlers();
 		loadStorageTypeOptions();
@@ -95,13 +96,16 @@ public class DownloadPreferencesDelegateImpl implements
 	private void setupAboutHandlers() {
 		final Uri uri = mPackageHelper.getMarketUri();
 		final Intent rateAppIntent = new Intent(Intent.ACTION_VIEW, uri);
-		mPreferenceManager.findPreference(ApplicationPreferences.KEY_RATE_APP).setIntent(rateAppIntent);
+		mPreferenceManager.findPreference(ApplicationPreferences.KEY_RATE_APP)
+				.setIntent(rateAppIntent);
 
 		final Intent aboutIntent = new Intent(mContext, AboutActivity.class);
-		mPreferenceManager.findPreference(ApplicationPreferences.KEY_ABOUT_ME).setIntent(aboutIntent);
+		mPreferenceManager.findPreference(ApplicationPreferences.KEY_ABOUT_ME)
+				.setIntent(aboutIntent);
 
 		final Intent donateIntent = new Intent(mContext, BuyAdFreeActivity.class);
-		final Preference donatePreference = mPreferenceManager.findPreference(ApplicationPreferences.KEY_DONATE);
+		final Preference donatePreference = mPreferenceManager.findPreference(
+				ApplicationPreferences.KEY_DONATE);
 
 		if (Config.Features.NEW_DONATE) {
 			throw new IllegalStateException("New Donate isn't implemented yet, doh!");
@@ -113,12 +117,9 @@ public class DownloadPreferencesDelegateImpl implements
 	}
 
 	private void startDownloadDirectoryChooser() {
-		final Intent chooseIntent = new Intent(mContext,
-				DirectoryChooserActivity.class);
-		chooseIntent.putExtra(DirectoryChooserActivity.EXTRA_NEW_DIR_NAME,
-				DOWNLOAD_DIRECTORY_NAME);
-		mActivityStarter.startActivityForResult(chooseIntent,
-				REQUEST_DOWNLOAD_DIRECTORY_CHOOSER);
+		final Intent chooseIntent = new Intent(mContext, DirectoryChooserActivity.class);
+		chooseIntent.putExtra(DirectoryChooserActivity.EXTRA_NEW_DIR_NAME, DOWNLOAD_DIRECTORY_NAME);
+		mActivityStarter.startActivityForResult(chooseIntent, REQUEST_DOWNLOAD_DIRECTORY_CHOOSER);
 	}
 
 	/*
@@ -129,21 +130,20 @@ public class DownloadPreferencesDelegateImpl implements
 	 * java.lang.String)
 	 */
 	@Override
-	public void onSharedPreferenceChanged(
-			final SharedPreferences sharedPreferences, final String key) {
+	public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences,
+			final String key) {
 
 		trackChange(sharedPreferences, key);
 		updateStorageTypeSummary();
 		mPathPreference.setSummary(mAppPreferences.getCustomPath());
-		mPathPreference
-				.setEnabled(mAppPreferences.getStorageType() == StorageType.CUSTOM);
+		mPathPreference.setEnabled(mAppPreferences.getStorageType() == StorageType.CUSTOM);
 	}
 
 	private void updateStorageTypeSummary() {
 		if (mAppPreferences.getStorageType() == StorageType.EXTERNAL) {
-			mTypePreference.setSummary(String.format("%s (%s)",
-					mAppPreferences.getStorageTypeDisplay(),
-					mAppPreferences.getStorageDirectory()));
+			mTypePreference.setSummary(
+					String.format("%s (%s)", mAppPreferences.getStorageTypeDisplay(),
+							mAppPreferences.getStorageDirectory()));
 		} else {
 			mTypePreference.setSummary(mAppPreferences.getStorageTypeDisplay());
 		}
@@ -155,20 +155,17 @@ public class DownloadPreferencesDelegateImpl implements
 	 * @param sharedPreferences
 	 * @param key
 	 */
-	private void trackChange(final SharedPreferences sharedPreferences,
-			final String key) {
+	private void trackChange(final SharedPreferences sharedPreferences, final String key) {
 		String value = null;
 
 		if (key == ApplicationPreferences.KEY_SSL_ENABLED) {
 			value = String.valueOf(sharedPreferences.getBoolean(key, false));
-		} else if (key == ApplicationPreferences.KEY_STORAGE_TYPE
-				|| key == ApplicationPreferences.KEY_STORAGE_CUSTOM_PATH) {
+		} else if (key == ApplicationPreferences.KEY_STORAGE_TYPE || key == ApplicationPreferences.KEY_STORAGE_CUSTOM_PATH) {
 			value = sharedPreferences.getString(key, "<undef>");
 		}
 
 		if (value != null) {
-			mTracker.sendEvent(ANALYTICS_TAG, "change",
-					String.format("%s:%s", key, value), null);
+			mTracker.sendEvent(ANALYTICS_TAG, "change", String.format("%s:%s", key, value), null);
 		}
 	}
 
@@ -195,34 +192,34 @@ public class DownloadPreferencesDelegateImpl implements
 	}
 
 	private void loadStorageTypeOptions() {
-		mTypePreference.setEntries(new CharSequence[] { getExternalLabel(),
-				getPhoneLabel(),
-				mContext.getString(R.string.storage_custom_label) });
-		mTypePreference.setEntryValues(new String[] {
-				StorageType.EXTERNAL.toString(), StorageType.LOCAL.toString(),
-				StorageType.CUSTOM.toString(), });
+		mTypePreference.setEntries(
+				new CharSequence[]{getExternalLabel(), getPhoneLabel(), mContext.getString(
+						R.string.storage_custom_label)});
+		mTypePreference.setEntryValues(
+				new String[]{StorageType.EXTERNAL.toString(), StorageType.LOCAL
+						.toString(), StorageType.CUSTOM.toString(),});
 
 		updateStorageTypeSummary();
 	}
 
 	private String getExternalLabel() {
-		final double free = getFreeExternalStorage() / Math.pow(1024, 3);
+		final long free = getFreeExternalStorage();
 
 		// This can be -1 or some other weird value on some Samsung crap devices.
 		if (free >= 0) {
-			return String.format(mContext.getString(R.string.storage_sd_label),
-					free);
+			final String freeStr = Formatter.formatShortFileSize(mContext, free);
+			return String.format(mContext.getString(R.string.storage_sd_label), freeStr);
 		}
 
 		return mContext.getString(R.string.storage_sd_label_no_free);
 	}
 
 	private String getPhoneLabel() {
-		final double free = getFreeInternalStorage() / Math.pow(1024, 3);
+		final long free = getFreeInternalStorage();
 
 		if (free >= 0) {
-			return String.format(
-					mContext.getString(R.string.storage_phone_label), free);
+			final String freeStr = Formatter.formatShortFileSize(mContext, free);
+			return String.format(mContext.getString(R.string.storage_phone_label), freeStr);
 		}
 
 		return mContext.getString(R.string.storage_phone_label_no_free);
@@ -243,18 +240,15 @@ public class DownloadPreferencesDelegateImpl implements
 	}
 
 	@Override
-	public void onActivityResult(final int requestCode, final int resultCode,
-			final Intent data) {
+	public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
 		Ln.i("onActivityResult: %d, %d, %s", requestCode, resultCode, data);
-		if (requestCode == REQUEST_DOWNLOAD_DIRECTORY_CHOOSER
-				&& resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
-			final String directory = data
-					.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR);
+		if (requestCode == REQUEST_DOWNLOAD_DIRECTORY_CHOOSER && resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
+			final String directory = data.getStringExtra(
+					DirectoryChooserActivity.RESULT_SELECTED_DIR);
 
 			updateCustomPath(directory);
 
-			Ln.i("New custom download path: %s",
-					mAppPreferences.getCustomPath());
+			Ln.i("New custom download path: %s", mAppPreferences.getCustomPath());
 		}
 	}
 
@@ -268,33 +262,31 @@ public class DownloadPreferencesDelegateImpl implements
 		}
 	}
 
-	private static class CustomPathChangeValidator implements
-			Preference.OnPreferenceChangeListener {
+	private static class CustomPathChangeValidator implements Preference.OnPreferenceChangeListener {
 
 		@Inject
 		private DownloadPathValidator mValidator;
 
 		@Override
-		public boolean onPreferenceChange(final Preference preference,
-				final Object newValue) {
+		public boolean onPreferenceChange(final Preference preference, final Object newValue) {
 			try {
 				mValidator.validateCustomPathOrThrow((String) newValue);
 			} catch (final DownloadPathValidationException e) {
 				int errorMsgId;
 
 				switch (e.getErrorCode()) {
-				case INSECURE_PATH:
-					errorMsgId = R.string.custom_path_error_insecure_path;
-					break;
-				case NOT_A_DIRECTORY:
-					errorMsgId = R.string.custom_path_error_not_a_directory;
-					break;
-				case PERMISSION_DENIED:
-					errorMsgId = R.string.custom_path_error_permission_denied;
-					break;
-				default:
-					errorMsgId = R.string.custom_path_error_unknown;
-					break;
+					case INSECURE_PATH:
+						errorMsgId = R.string.custom_path_error_insecure_path;
+						break;
+					case NOT_A_DIRECTORY:
+						errorMsgId = R.string.custom_path_error_not_a_directory;
+						break;
+					case PERMISSION_DENIED:
+						errorMsgId = R.string.custom_path_error_permission_denied;
+						break;
+					default:
+						errorMsgId = R.string.custom_path_error_unknown;
+						break;
 				}
 
 				showErrorDialog(preference.getContext(), errorMsgId);
@@ -308,7 +300,8 @@ public class DownloadPreferencesDelegateImpl implements
 			final Builder builder = new AlertDialog.Builder(context);
 			builder.setMessage(errorMsgId)
 					.setTitle(R.string.custom_path_error_title)
-					.setIcon(android.R.drawable.ic_dialog_alert).show();
+					.setIcon(android.R.drawable.ic_dialog_alert)
+					.show();
 		}
 	}
 }
