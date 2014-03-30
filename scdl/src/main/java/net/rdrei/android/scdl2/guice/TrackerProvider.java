0d41f300211f99edadafe036c9d78f6d8907474a
Pascal Hartig <passy@twitter.com>
2014-03-30 00:22:43 +0000

    Prepare release 1.8.0

--- scdl/src/main/java/net/rdrei/android/scdl2/guice/TrackerProvider.java
+++ scdl/src/main/java/net/rdrei/android/scdl2/guice/TrackerProvider.java
@@ -9,18 +9,15 @@ import com.google.inject.Provider;
 
 import net.rdrei.android.scdl2.R;
 
-import roboguice.util.Ln;
-
 public class TrackerProvider implements Provider<Tracker> {
-	
+
 	@Inject
 	private Context mContext;
 
 	@Override
 	public Tracker get() {
-		Ln.w("I CAN HAZ SINGLETONE?!?!?");
-
-		return GoogleAnalytics.getInstance(mContext).newTracker(R.xml.google_analytics);
+		final GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(mContext);
+		return googleAnalytics.newTracker(R.xml.google_analytics);
 	}
 
 }
