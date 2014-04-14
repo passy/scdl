2f89b64757d3ef9e4964de18a00c6754efed083f
Pascal Hartig <passy@twitter.com>
2014-04-14 19:58:58 +0100

    Fix heartbleed, v1.8.2

--- scdl/src/main/java/net/rdrei/android/scdl2/Config.java
+++ scdl/src/main/java/net/rdrei/android/scdl2/Config.java
@@ -5,15 +5,15 @@ public interface Config {
 	interface Features {
 		boolean PLAYLIST_DOWNLOADS = false;
 		boolean NEW_DONATE = false;
-	};
+	}
 
 	enum MARKETPLACE_TYPE {
 		GOOGLE_PLAY,
 		AMAZON
-	};
+	}
 
 	String API_CONSUMER_KEY = "429caab2811564cb27f52a7a4964269b";
-	String API_SSL_PIN_HASH = "0d0ff8901866394e55d1dde3a35aefa55b779db6";
+	String API_SSL_PIN_HASH = "e9fe522cd8dd960dacc046380f2ad6b7e979f687";
 	String TMP_DOWNLOAD_POSTFIX = ".tmp";
 	boolean PAID_BUILD = BuildConfig.PAID_BUILD;
 	MARKETPLACE_TYPE STORE = BuildConfig.STORE;
