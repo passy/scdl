82063a89dc77935201d69b448b0009a2cc688300
Pascal Hartig <passy@twitter.com>
2014-09-28 14:49:15 +0100

    Update API key docs
    Please use your own API key, when building the app. I didn't take the effort to
    filter-branch this out of the repo or squash the history, because these keys
    aren't really secret anyway. If you interecept any request, you can get it
    trivally anyway. In fact, you can use the Network Inspector in your favorite
    browser to see the API key of SoundCloud's webapp, if you like to and there's no
    secret part to the key. I expect these keys to get invalidated any time soon
    anyhow.

--- scdl/src/main/java/net/rdrei/android/scdl2/Config.java
+++ scdl/src/main/java/net/rdrei/android/scdl2/Config.java
@@ -12,7 +12,10 @@ public interface Config {
 		AMAZON
 	}
 
-	String API_CONSUMER_KEY = "429caab2811564cb27f52a7a4964269b";
+	// Get your SoundCloud API key from
+	// https://developers.soundcloud.com/docs/api/guide
+	String API_CONSUMER_KEY = "CONSUMER_KEY";
+	// The SSL PIN can be extracted via `contrib/pin.py`
 	String API_SSL_PIN_HASH = "e9fe522cd8dd960dacc046380f2ad6b7e979f687";
 	String TMP_DOWNLOAD_POSTFIX = ".tmp";
 	boolean PAID_BUILD = BuildConfig.PAID_BUILD;
